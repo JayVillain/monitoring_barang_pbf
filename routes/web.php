0bdc27878e0bcb8fe3b261db986e09b3bc4009de
JayVillain <saipi3825@gmail.com>
2026-04-20 14:55:47 +0000

    Apr 20, 2026, 9:55 PM

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\AuthController; // Ini nanti kita buat
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route yang hanya bisa diakses kalau sudah login
+Route::middleware(['auth'])->group(function () {
+    
+    // Resource route untuk CRUD barang
+    Route::resource('items', ItemController::class);
+
+    // Route untuk transaksi (Masuk/Keluar/Kembali)
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::post('/transactions/store', [TransactionController::class, 'store'])->name('transactions.store');
 });
